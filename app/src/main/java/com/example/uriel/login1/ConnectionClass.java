023bf471349d1de3c33e10273cce62bbe19c60fd
Uriel <usueldo@gmail.com>
2016-10-23 23:57:18 -0300

    Commit Message

---
+++ app/src/main/java/com/example/uriel/login1/ConnectionClass.java
@@ -0,0 +1,50 @@
+package com.example.uriel.login1;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionClass {
+    String ip = "db4free.net";
+    String port = "3306";
+    String classs = "com.mysql.jdbc.Driver";
+    String db = "android808";
+    String un = "admin808";
+    String password = "123456";
+    String error ="";
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Class.forName(classs);
+
+            ConnURL = "jdbc:mysql://" + ip + ":"
+                    + port + "/" + db;
+
+
+
+            conn = DriverManager.getConnection(ConnURL,un,password);
+          //  conn = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/android808", "admin808", "123456");
+
+        } catch (SQLException se) {
+            error = se.getMessage();
+            System.out.println(conn);
+            se.printStackTrace();
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
+
---
+++ app/src/main/java/com/example/uriel/login1/login.java
@@ -0,0 +1,103 @@
+package com.example.uriel.login1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class login extends AppCompatActivity {
+
+    ConnectionClass connectionClass;
+    EditText et_user, et_password;
+    Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        connectionClass = new ConnectionClass();
+        et_user = (EditText)findViewById(R.id.et_user);
+        et_password = (EditText)findViewById(R.id.et_password);
+        btn_login = (Button)findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DoLogin  doLogin = new DoLogin();
+                doLogin.execute("");
+            }
+        });
+    }
+
+
+    public class DoLogin extends AsyncTask<String,String,String>
+    {
+        String z = "";
+        Boolean isSuccess = false;
+
+
+        String userid = et_user.getText().toString();
+        String password = et_password.getText().toString();
+
+        @Override
+        protected void onPostExecute(String r) {
+            Toast.makeText(login.this,r,Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(userid.trim().equals("")|| password.trim().equals(""))
+                z = "Please enter User Id and Password";
+            else
+            {
+                try {
+                    Connection con = connectionClass.CONN();
+                    if (con == null) {
+                        z = "Error in connection with SQL server";
+                    } else {
+
+                        String query = "select * from users where user_id='" + userid + "' and user_pass='" + password + "'";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+                        if(rs.next())
+                        {
+                            String locked = "";
+                            locked = null;
+                            locked = rs.getString("locked");
+                            if (locked == null){
+                                z = "Login successfull";
+                            }else{
+                                z = "User locked";
+                            }
+
+                            isSuccess=true;
+                        }
+                        else
+                        {
+                            z = "User or Pass wrong";
+                            isSuccess = false;
+                        }
+                        con.close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    isSuccess = false;
+                    z = "Exceptions";
+                }
+            }
+            return z;
+        }
+
+    }
+}
