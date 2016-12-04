41d668baf6b547545d2ba713636d6657d8492502
nsueldo <nmsueldo@gmail.com>
2016-12-03 22:55:49 -0300

    Shared Preferences

--- app/src/main/java/com/example/uriel/login1/onStartUp.java
+++ app/src/main/java/com/example/uriel/login1/onStartUp.java
@@ -17,16 +17,7 @@ public class onStartUp extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         //Show Toast when the charger connects
         Toast.makeText(context, "Ha conectado el cargador.", Toast.LENGTH_SHORT).show();
-        //Instance Travels Receiver Class
-        Intent iTravelReceiver = new Intent(context, TravelsReceiver.class);
-        final PendingIntent pIntent = PendingIntent.getBroadcast(context,
-                TravelsReceiver.REQUEST_CODE, iTravelReceiver, PendingIntent.FLAG_UPDATE_CURRENT);
-        //Get Current Time in Milliseconds
-        long firstMillis = System.currentTimeMillis();
-        //Starting of Current Time -> Set Alarm every 45 Seconds
-        AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis,
-                    45000L, pIntent);
+        //Set Alarm "Notifications"
     }
 }
 
--- app/src/main/java/com/example/uriel/login1/settings.java
+++ app/src/main/java/com/example/uriel/login1/settings.java
@@ -1,27 +1,13 @@
 package com.example.uriel.login1;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.CheckBox;
 import android.widget.TextView;
-import java.util.ArrayList;
-import android.view.Gravity;
-
-import android.widget.Toast;
 
 public class settings extends AppCompatActivity {
+    //UI Declarations
     Switch notification;
     TextView status;
 
@@ -29,29 +15,25 @@ public class settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        //Set Screen Settings
         setSettings();
-
-        //Set Action "On Checked Changed" for Button Switch
+        //Activate Action "On Checked Changed" for Button Switch
         status = (TextView) findViewById(R.id.status);
         notification = (Switch) findViewById(R.id.notification);
         notification.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
-                    status.setText("Switch is currently ON");
+                    //Enable Travel Notifications
+                    alarmClass.setNotifications(getApplicationContext());
+                    status.setText("Notificacines de Viajes: Prendidas");
                 } else{
-                    //Get Alarm System Servide
-                    AlarmManager aManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-                    //Get Intent TravelsReceiver
-                    Intent intent = new Intent(getBaseContext(), TravelsReceiver.class);
-                    //Build Pending Intent from the Intent
-                    PendingIntent pIntent = PendingIntent.getBroadcast(getApplicationContext(),
-                            TravelsReceiver.REQUEST_CODE, intent,
-                            PendingIntent.FLAG_UPDATE_CURRENT);
-                    //Cancel de Pending Item of Alarm Manager
-                    aManager.cancel(pIntent);
-                    status.setText("Switch is currently OFF");
+                    //Disable Travel Notifications
+                    alarmClass.disableNotifications(getApplicationContext());
+                    status.setText("Notificacines de Viajes: Apagadas");
                 }
+                //Save Travel Notifications Settings in Shared Preferences
+                sharedPreferences.saveSettings(getApplicationContext(), isChecked);
             }
        });
     }
