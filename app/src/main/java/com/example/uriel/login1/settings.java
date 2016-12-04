02763de278a82c80f1d56446dee713ec7ddab57b
nsueldo <nmsueldo@gmail.com>
2016-12-03 22:37:37 -0300

    Shared Preferences

--- app/src/main/java/com/example/uriel/login1/settings.java
+++ app/src/main/java/com/example/uriel/login1/settings.java
@@ -24,26 +24,22 @@ import android.widget.Toast;
 public class settings extends AppCompatActivity {
     Switch notification;
     TextView status;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setSettings();
 
+        //Set Action "On Checked Changed" for Button Switch
         status = (TextView) findViewById(R.id.status);
         notification = (Switch) findViewById(R.id.notification);
-
-
-
-        notification.setChecked(true);
         notification.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
             @Override
-            public void onCheckedChanged(CompoundButton buttonView,
-                                         boolean isChecked) {
-
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
                     status.setText("Switch is currently ON");
-                }else{
+                } else{
                     //Get Alarm System Servide
                     AlarmManager aManager = (AlarmManager) getSystemService(ALARM_SERVICE);
                     //Get Intent TravelsReceiver
@@ -58,5 +54,15 @@ public class settings extends AppCompatActivity {
                 }
             }
        });
+    }
+
+    private void setSettings(){
+        //UI Declarations
+        boolean checkNotification;
+        //Get Shared Preferences of Button Switch
+        checkNotification = sharedPreferences.getSettings(getApplicationContext());
+        //Set Preferences in the Screen
+        notification = (Switch) findViewById(R.id.notification);
+        notification.setChecked(checkNotification);
     }
 }
--- app/src/main/java/com/example/uriel/login1/sharedPreferences.java
+++ app/src/main/java/com/example/uriel/login1/sharedPreferences.java
@@ -21,4 +21,20 @@ public class sharedPreferences {
         data[1] = sp1.getString("PASSWORD", null);
         return data;
     }
+
+    static public void saveSettings (Context context, boolean statusNotify){
+        SharedPreferences sp = context.getSharedPreferences("Settings", 1);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("NOTIFICATIONS", statusNotify);
+        editor.commit();
+    }
+
+    static public boolean getSettings (Context context){
+        boolean isChecked;
+        SharedPreferences sp = context.getSharedPreferences("Settings", 1);
+        isChecked = sp.getBoolean("NOTIFICATIONS", false);
+        return isChecked;
+    }
+
+
 }
